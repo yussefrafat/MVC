5013be474ba1bc4fbbde5b2c2eefe37ec7c763d9
Yussef Rafat <yussefrafat5908@gmail.com>
2021-09-12 12:40:21 -0400

    final touches

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWDB_URL) {
-    sequelize = new Sequelize(process.env.JAWDB_URL);
+//use jawsdb if availible else create new database
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306 
-    });
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 }
 
-module.exports = sequelize
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,40 +1,49 @@
+//imports
 const express = require('express');
 const sequelize = require('./config/connection')
 const routes = require('./controllers')
-const path = require('path');
-const exp = require('constants');
+const path = require('path')
 const app = express();
 const helpers = require('./utils/helpers');
 const PORT = process.env.PORT || 3001;
 
-
+//middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//view engine
 const exphbs = require('express-handlebars');
-const { Console } = require('console');
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
+
+//session
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: '53cr37',
-    cookie: {},
-    resave: false,
-    saveUnitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 5,
-        expiration: 1000 * 60 * 15
-    }),
+  secret: '53cr37',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+    checkExpirationInterval: 1000 * 60 * 5, //checks if user is idle every 5 minutes
+    expiration: 1000 * 60 * 15 //if user is idle for 15 minutes the session ends
+  }),
+
 };
+//
 
 app.use(session(sess));
 
+// turn on routes
 app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-   app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
